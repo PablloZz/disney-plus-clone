5c244fc6b0a0a5c8adb54d2fc3caf449928bba68
Pavlo Zalutskiy <kyrogane1@gmail.com>
2022-10-22 14:51:30 +0300

    ADD: detail page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
-
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import {Routes, Route} from "react-router-dom"
+import Detail from "./components/Detail";
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail" element={<Detail />} />
+      </Routes>
     </div>
   );
 }
